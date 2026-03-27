9e0395ead571ce990233c51300246ba35df0a234
emilie-ng <emilienguyen2807@gmail.com>
2026-03-27 15:55:40 +0100

    boucle de traitement de plusieurs automates dans main

--- main.py
+++ main.py
@@ -6,58 +6,64 @@ from minimisation import *
 from reconnaissance import *
 from complementaire import *
 
-#saisie de l'automate
-print("Quel automate voulez-vous tester ?")
-automate = input("Saisie : ")
-print()
+automate = ""
 
-#affichage de l'automate
-print("Affichage de l'automate :")
-print()
-AF = lire_automate_sur_fichier(automate)
-afficher_automate(AF)
-print()
-print()
+while automate != "stop" :
+    #saisie de l'automate
+    print("Quel automate voulez-vous tester ?")
+    print("Pour arrêter le test, tapez 'stop'.")
+    automate = input("Saisie : ")
+    if automate == "stop" :
+        break
+    print()
 
-#standardisation
-print("Standardisation :")
-print()
-AFS = est_standard(AF)
-afficher_automate(AFS)
-print()
-print()
+    #affichage de l'automate
+    print("Affichage de l'automate :")
+    print()
+    AF = lire_automate_sur_fichier(automate)
+    afficher_automate(AF)
+    print()
+    print()
 
-#déterminisation et complétion
-print("Déterminisation et complétion :")
-print()
-AFDC = determinisation_final(AF)
-print()
-print()
+    #standardisation
+    print("Standardisation :")
+    print()
+    AFS = est_standard(AF)
+    afficher_automate(AFS)
+    print()
+    print()
 
-#minimisation
-print("Minimisation :")
-print()
-AFDCM = minimisation(AFDC)
-print()
-afficher_automate_minimal(AFDC, AFDCM)
-print()
-print()
+    #déterminisation et complétion
+    print("Déterminisation et complétion :")
+    print()
+    AFDC = determinisation_final(AF)
+    print()
+    print()
 
-#reconnaissance de mots
-print("Reconnaissance de mots : ")
-print()
-print("Pour arrêter les saisies, entrer 'fin'.")
-mot = input("Saisir un mot : ")
-while mot != "fin" :
-    print(reconnaitre_mot(mot, AFDC))
+    #minimisation
+    print("Minimisation :")
+    print()
+    AFDCM = minimisation(AFDC)
+    print()
+    afficher_automate_minimal(AFDC, AFDCM)
+    print()
+    print()
+
+    #reconnaissance de mots
+    print("Reconnaissance de mots : ")
+    print()
+    print("Pour arrêter les saisies, entrer 'fin'.")
     mot = input("Saisir un mot : ")
-print()
-print()
+    while mot != "fin" :
+        print(reconnaitre_mot(mot, AFDC))
+        mot = input("Saisir un mot : ")
+    print()
+    print()
 
-#langage complémentaire
-print("Langage complémentaire :")
-print()
-AComp = automate_complementaire(AFDC)
-afficher_automate(AComp)
-print()
-print()
+    #langage complémentaire
+    print("Langage complémentaire :")
+    print()
+    AComp = automate_complementaire(AFDC)
+    afficher_automate(AComp)
+    print()
+    print()
