a86e6220e2a50aeef074fe5403d89a2bce57726a
CollynMarguerite <marguerite.collyn@gmail.com>
2026-03-27 13:02:54 +0100

    Standardisation FINI

--- standardisation.py
+++ standardisation.py
@@ -1,38 +1,50 @@
 def standardisation(af):
-    #Créer un nouvel état 'I' 
-    #Ajouter toutes les transitions des états iniiaux à 'I' qui est devenu l'état initial
-    #Automate 6: anciens états initiaux 1 et 3 devenus inutiles(Aucun chemin ne mène à eux)
     nouvel_etat = 'I'
-    af["etats"].insert(0, nouvel_etat)#Ajout de l'état I
+    af_std = af.copy()
+    af_std['etats'] = [nouvel_etat]#Ajout de l'état initial dans le nouvel automate
+    af_std['initial'] = [nouvel_etat]#Ajout de l'état initial dans le nouvel automate
+    af_std['transitions'] = []
+    etat_afstd = []#etats utiles qu'on vas rajouter à la fin
     for tr in af['transitions']:
         if tr[0] == nouvel_etat:
-            continue #Evite d'ajouter les mêmes transitions(car les transitions de I fera partie de la boucle)
+            continue  # Evite d'ajouter les mêmes transitions(car les transitions de I fera partie de la boucle)
         elif tr[0] in af['initial']:
-            af['transitions'].append([nouvel_etat, tr[1], tr[2]])#ajout de la transition de I
-    #print(af)
-    af["initial"] = None
-    af["initial"] = [nouvel_etat]
-    #print(af)
-    #supp_etat = []
-    """for i in af['transitions']:
-        supp_etat.append(i[0])
-        supp_etat.append(i[2])
-    for e in af['etats']:
-        if e not in supp_etat:
-            af['etats'].remove(e)"""
-    return af
+            af_std['transitions'].append([nouvel_etat, tr[1], tr[2]]) # ajout de la transition de I le nouvel état initial
+            if tr[2] not in etat_afstd:
+                etat_afstd.append(tr[2])
+            if tr[0] == tr[2]:#si l'état reboucle sur lui même, on garde la transition
+                af_std['transitions'].append(tr)
+                if tr[2] not in etat_afstd:
+                    etat_afstd.append(tr[2])
+            if tr[0] in af['initial'] and tr[0] in af['final']:#si l'ancienne état initial est final, I doit être final aussi
+                af_std['final'].append(nouvel_etat)
+                af_std['transitions'].append(tr)#ajout de la transition de l'ancien état initial
+                if tr[0] not in etat_afstd:
+                    etat_afstd.append(tr[0])
+        else:
+            af_std['transitions'].append(tr)#ajout des transitions intermédiaires
+            if tr[2] not in etat_afstd and tr[0] not in etat_afstd:
+                etat_afstd.extend([tr[0], tr[2]])
+        if tr[2] in af['initial'] and tr not in af['transitions']:
+            af_std['transitions'].append(tr)
+            """if tr[0] != tr[2]:
+                transition_supp.append(tr)"""
+    af_std['etats'] += sorted(etat_afstd)#ajout des etats utiles dans l'ordre croissant
+    return af_std
+
 
 
 def est_standard(af):
-    if len(af['initial']) > 1 or any(tr[2] in af['initial'] for tr in af['transitions']):
+    if len(af['initial']) > 1 or any(tr[2] in af['initial'] for tr in af['transitions']):#conditions de la standardisation
         print(af)
         print("Automate non standard")
         ans = input("Voulez-vous le standardisez? ecrivez OUI ou NON: ")
         if ans == "NON":
             return "Automate non standard"
         else:
-            standardisation(af)
-            print(af)
-            return "Automate standardisée"
+            af_std = standardisation(af)
+            print(af_std)
+            return "Automate standardisée!"
     else:
         return "Automate standard"
+
